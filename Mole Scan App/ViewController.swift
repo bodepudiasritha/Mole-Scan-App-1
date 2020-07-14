4c0c3f151d2903208860f1e3316ef8d429a92dfb
Vidushi <meelvidushi1@gmail.com>
2020-07-14 18:03:15 -0400

    Initial Commit

---
+++ Mole Scan App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mole Scan App
+//
+//  Created by Vidushi Meel on 7/14/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
